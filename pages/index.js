48d6e82278b5ac1325ff719be63a1a467fd5d9c6
curlyzik <curlyzik@gmail.com>
2022-01-06 00:43:14 +0100

    new changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Search from "../components/Search";
 import { Select } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   useGetUniversitiesQuery,
   useGetUniversityByIdQuery,
@@ -8,29 +8,67 @@ import {
 import axios from "axios";
 const { Option } = Select;
 
-export default function Home({
-  universities,
-  departments,
-  courses,
-  years,
-  levels,
-  semesters,
-}) {
-  const [value, setValue] = useState("");
-  // const { data, error, isLoading } = useGetUniversitiesQuery();
+export default function Home() {
+  const [universities, setUniversities] = useState();
+  const [departments, setDepartments] = useState();
+  const [courses, setCourses] = useState();
+  const [years, setYears] = useState();
+  const [levels, setLevels] = useState();
+  const [semesters, setSemesters] = useState();
 
-  console.log(universities, departments, courses, years, levels, semesters);
+  const [name, setName] = useState()
+
+  useEffect(() => {
+    const getUrls = async () => {
+      const [
+        universityRes,
+        departmentRes,
+        courseRes,
+        yearRes,
+        levelRes,
+        semesterRes,
+      ] = await Promise.all([
+        fetch(`http://localhost:8000/universities/`),
+        fetch("http://localhost:8000/department/"),
+        fetch("http://localhost:8000/course/"),
+        fetch("http://localhost:8000/year/"),
+        fetch("http://localhost:8000/level/"),
+        fetch("http://localhost:8000/semester/"),
+      ]);
+
+      const [universities, departments, courses, years, levels, semesters] =
+        await Promise.all([
+          universityRes.json(),
+          departmentRes.json(),
+          courseRes.json(),
+          yearRes.json(),
+          levelRes.json(),
+          semesterRes.json(),
+        ]);
+
+      setUniversities(universities);
+      setDepartments(departments);
+      setCourses(courses);
+      setYears(years);
+      setLevels(levels);
+      setSemesters(semesters);
+    };
+
+    getUrls();
+  }, []);
+
+  console.log(universities)
 
   const handleChange = (value) => {
-    setValue(value);
-    console.log(value, 'Selected');
+    setValue(name);
+    console.log(name, "Selected");
   };
   return (
     <div className="flex flex-col gap-y-7">
       <h1 className="text-4xl">Select Past Question</h1>
       <div className="grid grid-cols-3 gap-3">
         <Search handleChange={handleChange} description={"Select University"}>
-          {universities.map((university) => (
+          {universities?.map((university) => (
             <Option key={university.id} value={university.name}>
               {university.name}
             </Option>
@@ -38,7 +76,7 @@ export default function Home({
         </Search>
 
         <Search handleChange={handleChange} description={"Select Department"}>
-          {departments.map((department) => (
+          {departments?.map((department) => (
             <Option key={department.id} value={department.name}>
               {department.name}
             </Option>
@@ -46,7 +84,7 @@ export default function Home({
         </Search>
 
         <Search handleChange={handleChange} description={"Select Courses"}>
-          {courses.map((course) => (
+          {courses?.map((course) => (
             <Option key={course.id} value={course.name}>
               {course.name}
             </Option>
@@ -54,7 +92,7 @@ export default function Home({
         </Search>
 
         <Search handleChange={handleChange} description={"Select Year"}>
-          {years.map((year) => (
+          {years?.map((year) => (
             <Option key={year.id} value={year.year}>
               {year.year}
             </Option>
@@ -62,7 +100,7 @@ export default function Home({
         </Search>
 
         <Search handleChange={handleChange} description={"Select level"}>
-          {levels.map((level) => (
+          {levels?.map((level) => (
             <Option key={level.id} value={level.level}>
               {level.level}
             </Option>
@@ -70,7 +108,7 @@ export default function Home({
         </Search>
 
         <Search handleChange={handleChange} description={"Select semester"}>
-          {semesters.map((semester) => (
+          {semesters?.map((semester) => (
             <Option key={semester.id} value={semester.semester}>
               {semester.semester}
             </Option>
@@ -81,16 +119,23 @@ export default function Home({
   );
 }
 
-export async function getServerSideProps(context) {
-  const [universityRes, departmentRes, courseRes, yearRes, levelRes, semesterRes] =
-    await Promise.all([
-      fetch("http://localhost:8000/universities/"),
-      fetch("http://localhost:8000/department/"),
-      fetch("http://localhost:8000/course/"),
-      fetch("http://localhost:8000/year/"),
-      fetch("http://localhost:8000/level/"),
-      fetch("http://localhost:8000/semester/"),
-    ]);
+export async function getServerSideProps({ query }) {
+  console.log(query);
+  const [
+    universityRes,
+    departmentRes,
+    courseRes,
+    yearRes,
+    levelRes,
+    semesterRes,
+  ] = await Promise.all([
+    fetch("http://localhost:8000/universities/"),
+    fetch("http://localhost:8000/department/"),
+    fetch("http://localhost:8000/course/"),
+    fetch("http://localhost:8000/year/"),
+    fetch("http://localhost:8000/level/"),
+    fetch("http://localhost:8000/semester/"),
+  ]);
 
   const [universities, departments, courses, years, levels, semesters] =
     await Promise.all([
