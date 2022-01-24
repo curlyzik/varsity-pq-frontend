78be049b709d1e4b701831465a83b14e038a0c9f
curlyzik <curlyzik@gmail.com>
2022-01-24 22:34:52 +0100

    cleaned up files

--- src/app/store.js
+++ src/app/store.js
@@ -4,20 +4,16 @@ import { facultyApi } from "../services/faculty";
 import { universityApi } from "../services/university";
 import { yearApi } from "../services/year";
 import { levelApi } from "../services/level";
-import { courseApi } from "../services/course";
 import { semesterApi } from "../services/semester";
-import { pastQuestionApi } from "../services/pastQuestion";
 
 export const store = configureStore({
   reducer: {
     [universityApi.reducerPath]: universityApi.reducer,
     [facultyApi.reducerPath]: facultyApi.reducer,
     [departmentApi.reducerPath]: departmentApi.reducer,
-    [courseApi.reducerPath]: courseApi.reducer,
     [yearApi.reducerPath]: yearApi.reducer,
     [levelApi.reducerPath]: levelApi.reducer,
     [semesterApi.reducerPath]: semesterApi.reducer,
-    [pastQuestionApi.reducerPath]: pastQuestionApi.reducer,
   },
   
   middleware: (getDefaultMiddleware) =>
@@ -25,10 +21,8 @@ export const store = configureStore({
       universityApi.middleware,
       facultyApi.middleware,
       departmentApi.middleware,
-      courseApi.middleware,
       yearApi.middleware,
       levelApi.middleware,
       semesterApi.middleware,
-      pastQuestionApi.middleware,
     ),
 });
