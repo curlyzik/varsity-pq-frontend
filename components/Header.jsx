d7dc69c1b11cdd3bfdc07584dfa199fa6a779554
curlyzik <curlyzik@gmail.com>
2022-02-03 15:58:37 +0100

    i de try fix the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ import { Btn } from ".";
 const Header = () => {
   return (
     <div className="bg-heroBG font-body overflow-hidden py-4 px-4">
-      <div className="mx-4 flex grid-cols-[30%,70%] items-center justify-between text-white lg:grid">
+      <div className="mx-4 flex grid-cols-[30%,70%] justify-between text-white lg:grid">
         <Link href="/">
           <a>
             <div className="text-3xl font-bold">Vartsity PQ</div>
