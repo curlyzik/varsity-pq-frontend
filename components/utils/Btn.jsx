593e4ca1f80b5680975059753fe4a46fc3cbe425
curlyzik <curlyzik@gmail.com>
2022-02-05 01:14:53 +0100

    add uni data to database

--- components/utils/Btn.jsx
+++ components/utils/Btn.jsx
@@ -3,7 +3,7 @@ import Ripples from "react-ripples";
 
 const Btn = ({ children, classNames, onClick }) => {
   return (
-    <Ripples className={`font-semibold capitalize ${classNames}`}>
+    <Ripples className={`font-semibold capitalize ${classNames}`} onClick={onClick}>
       {children}
     </Ripples>
   );
