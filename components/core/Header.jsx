4e2af2d5df9a2a66f10bbeee5315ea3a32589fc8
curlyzik <curlyzik@gmail.com>
2022-02-16 01:31:19 +0100

    fix link colors

--- components/core/Header.jsx
+++ components/core/Header.jsx
@@ -8,12 +8,12 @@ import { Btn } from "../index";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="relative border-b border-b-gray-600 bg-black !text-white">
+    <div className="relative border-b border-b-gray-600 bg-black">
       <div className="font-body  overflow-hidden bg-[#00044e] bg-opacity-60 py-4 px-4">
         <div className="mx-4 flex items-center justify-between !text-white">
           <div>
             <Link href="/">
-              <a>
+              <a className="!text-white">
                 <div
                   className="text-3xl font-bold text-white"
                   onClick={() => setIsOpen(false)}
@@ -23,31 +23,31 @@ const Header = () => {
               </a>
             </Link>
           </div>
-          <ul className="hidden items-center justify-center gap-x-4 text-base !text-white lg:flex">
+          <ul className="hidden items-center justify-center gap-x-4 text-base lg:flex">
             <li>
-              <a>How it works</a>
+              <a className="!text-white">How it works</a>
             </li>
             <li>
-              <a href="http://varsity-pq.herokuapp.com/" target={"_blank"}>
+              <a className="!text-white" href="http://varsity-pq.herokuapp.com/" target={"_blank"}>
                 API
               </a>
             </li>
             <li>
               <Link href={"/volunteer/volunteer-request"}>
-                <a>Admin/Volunteer Request</a>
+                <a className="!text-white">Admin/Volunteer Request</a>
               </Link>
             </li>
             <li>
-              <a>Contributors</a>
+              <a className="!text-white">Contributors</a>
             </li>
             <li>
-              <a>Past Questions</a>
+              <a className="!text-white">Past Questions</a>
             </li>
             <li>
-              <a>Collections</a>
+              <a className="!text-white">Collections</a>
             </li>
             <li>
-              <a>
+              <a className="!text-white">
                 <Btn
                   classNames={
                     "border px-2 py-[4px] text-blue-400 border-blue-400"
@@ -77,52 +77,52 @@ const Header = () => {
           <div className="absolute inset-0 z-20 mt-16 border-t border-t-gray-500 !text-white lg:hidden">
             <div className="bg-black">
               <div className="w-full bg-[#00044e] bg-opacity-60 py-8 px-4">
-                <ul className="flex flex-col items-start justify-center gap-y-7 gap-x-4 text-lg !text-white">
+                <ul className="flex flex-col items-start justify-center gap-y-7 gap-x-4 text-lg">
                   <Link href={"/"}>
                     <li
-                      className="w-full border-b border-b-gray-400 pb-1 text-white"
+                      className="w-full border-b border-b-gray-400 pb-1"
                       onClick={() => setIsOpen(false)}
                     >
-                      <a>How it works</a>
+                      <a className="!text-white">How it works</a>
                     </li>
                   </Link>
                   <li
                     className="w-full border-b border-b-gray-400 pb-1 text-white"
                     onClick={() => setIsOpen(false)}
                   >
-                    <a href="http://varsity-pq.herokuapp.com/">PI</a>
+                    <a className="!text-white" href="http://varsity-pq.herokuapp.com/">API</a>
                   </li>
                   <li
-                    className="w-full border-b border-b-gray-400 pb-1 text-white"
+                    className="w-full border-b border-b-gray-400 pb-1"
                     onClick={() => setIsOpen(false)}
                   >
                     <Link href={"/volunteer/volunteer-request"}>
-                      <a>Admin/Volunteer Request</a>
+                      <a className="!text-white">Admin/Volunteer Request</a>
                     </Link>
                   </li>
                   <li
-                    className="w-full border-b border-b-gray-400 pb-1 text-white"
+                    className="w-full border-b border-b-gray-400 pb-1"
                     onClick={() => setIsOpen(false)}
                   >
-                    <a>Contributors</a>
+                    <a className="!text-white">Contributors</a>
                   </li>
                   <li
-                    className="w-full border-b border-b-gray-400 pb-1 text-white"
+                    className="w-full border-b border-b-gray-400 pb-1"
                     onClick={() => setIsOpen(false)}
                   >
-                    <a>Past Questions</a>
+                    <a className="!text-white">Past Questions</a>
                   </li>
                   <li
-                    className="w-full border-b border-b-gray-400 pb-1 text-white"
+                    className="w-full border-b border-b-gray-400 pb-1"
                     onClick={() => setIsOpen(false)}
                   >
-                    <a>Collections</a>
+                    <a className="!text-white">Collections</a>
                   </li>
                   <li
-                    className="w-full border-b border-b-gray-400 pb-1 text-white"
+                    className="w-full border-b border-b-gray-400 pb-1"
                     onClick={() => setIsOpen(false)}
                   >
-                    <a>New Past Question</a>
+                    <a className="!text-white">New Past Question</a>
                   </li>
                 </ul>
               </div>
