513166e136f0cbea039fadb29531af7cbe9d1bc3
curlyzik <curlyzik@gmail.com>
2022-02-16 04:03:20 +0100

    edited modal

--- components/core/Modal.jsx
+++ components/core/Modal.jsx
@@ -9,11 +9,17 @@ const Modal = ({ children, isModalVisible, handleOk, handleCancel }) => {
         visible={isModalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
+        className="!px-6 md:px-0"
         footer={[
           <Button key="back" onClick={handleCancel}>
             Cancel
           </Button>,
-          <Button key="cancle" type="primary" onClick={handleOk} className="text-black">
+          <Button
+            key="cancel"
+            type="primary"
+            onClick={handleOk}
+            className="text-black"
+          >
             OK
           </Button>,
         ]}
