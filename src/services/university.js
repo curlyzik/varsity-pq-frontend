c29ea29f976aae25439963c4ed093ce766b5a857
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2022-03-23 00:09:40 +0100

    configured rapid api for university

--- src/services/university.js
+++ src/services/university.js
@@ -3,14 +3,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const universityApi = createApi({
   reducerPath: "universityApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/universities/`,
+    baseUrl: `${process.env.NEXT_PUBLIC_UNIVERSITY_API_URL}`,
   }),
   endpoints: (builder) => ({
     getUniversities: builder.query({
       query: () => ({
         url: "",
         headers: {
-          Authorization: `Token ${process.env.NEXT_PUBLIC_TOKEN}`,
+          "X-RapidAPI-Host": process.env.NEXT_PUBLIC_UNIVERSITY_API_HOST,
+          "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
         },
       }),
     }),
