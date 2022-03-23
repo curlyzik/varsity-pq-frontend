01248fccbb12cd38902dcb062c76f82ab2a8c82a
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2022-03-23 03:18:49 +0100

    updated view

--- src/services/pastquestion.js
+++ src/services/pastquestion.js
@@ -1,13 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const createQuery = (url) => ({
+  url,
+  headers: {
+    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_PQ_API_HOST,
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+  },
+});
+
 export const pastQuestionApi = createApi({
   reducerPath: "pastQuestionApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/past_question/`,
+    baseUrl: `${process.env.NEXT_PUBLIC_PQ_API_URL}`,
   }),
   endpoints: (builder) => ({
     getPastQuestions: builder.query({
-      query: () => "",
+      query: () => createQuery("/"),
     }),
   }),
 });
