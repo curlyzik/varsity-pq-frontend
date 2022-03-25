6d03aa2fbff9215036ee624b7e0158db920ffef8
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2022-03-25 19:07:46 +0100

    seo

--- components/core/MetaTags.jsx
+++ components/core/MetaTags.jsx
@@ -9,26 +9,29 @@ const MetaTags = () => {
       />
       <meta name="theme-color" content="#00044e" />
 
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
       <title>
         Varsity PQ - Store House of 300+ curated past questions from 160
         universites in Nigeria.
       </title>
       <meta
         name="description"
-        content="Varsity PQ Varsity PQ is the all in one storehouse where students from various universities could find past questions to different courses of their discipline online. Varsity PQ is made up of 300+ curated past questions from 170 universites in Nigeria."
+        content="Varsity PQ is the all in one storehouse where students from various universities could find past questions to different courses of their discipline online. Varsity PQ is made up of 300+ curated past questions from 170 universites in Nigeria."
       />
       <meta
         name="keywords"
-        content="past questions, jamb, university, past question, admission, universities, Varsity PQ by Isaac Nzekwe, varsitypq, VARSITYPQ, exam past questions"
+        content="past questions, jamb, university, past questions, admission, universities, varsity pq by Isaac Nzekwe, varsitypq.com, varsity pq, exam past questions, past-questions, materials"
       />
-      <link rel="canonical" href="http://varsity-pq-frontend.vercel.app/" />
+      <link rel="canonical" href="http://varsitypq.com/" />
       <link rel="apple-touch-icon" href="/favicon.png" />
       <link rel="icon" href="/favicon.png" />
 
       {/* Primary Meta Tags */}
       <meta
         name="title"
-        content="Varsity PQ - Store House of 300+ Developer Cheatsheets"
+        content="Varsity PQ - Store House of 300+ Past Questions"
       />
       <meta
         name="description"
@@ -37,13 +40,10 @@ const MetaTags = () => {
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta
-        property="og:url"
-        content="http://varsity-pq-frontend.vercel.app/"
-      />
+      <meta property="og:url" content="http://varsitypq.com/" />
       <meta
         property="og:title"
-        content="Varsity PQ - Store House of 300+ Developer Cheatsheets"
+        content="Varsity PQ - Store House of 300+ Past Questions"
       />
       <meta
         property="og:description"
@@ -51,18 +51,16 @@ const MetaTags = () => {
       />
       <meta
         property="og:image"
-        content="https://varsity-pq-frontend.vercel.app/varsity-pq-banner.png"
+        content="https://varsitypq.com/varsity-pq-banner.png"
       />
 
       {/* Twitter */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta
-        property="twitter:url"
-        content="http://varsity-pq-frontend.vercel.app/"
-      />
+      <meta property="twitter:site" content="@VarsityPQ_" />
+      <meta property="twitter:creator" content="@curlyzik" />
+      <meta property="twitter:url" content="http://varsitypq.com/" />
       <meta
         property="twitter:title"
-        content="Varsity PQ - Store House of 300+ Developer Cheatsheets"
+        content="Varsity PQ - Store House of 300+ Past Questions"
       />
       <meta
         property="twitter:description"
@@ -70,22 +68,12 @@ const MetaTags = () => {
       />
       <meta
         property="twitter:image"
-        content="https://varsity-pq-frontend.vercel.app/varsity-pq-banner.png"
+        content="https://varsitypq.com/varsity-pq-banner.png"
       />
-
-      {/* Buy Me A Coffee
-      <script
-        data-name="BMC-Widget"
-        data-cfasync="false"
-        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-        data-id="Isaac Nzekwe"
-        data-description="Support me on Buy me a coffee!"
-        data-message="If you're enjoying my app, consider supporting me with a coffee ☕️"
-        data-color="#FFDD00"
-        data-position="Right"
-        data-x_margin="18"
-        data-y_margin="18"
-      ></script> */}
+      <meta
+        property="twitter:card"
+        content="summary_large_image"
+      />
     </Head>
   );
 };
