9bb0e2edcc3c90b315a774d7cb4b9267c31b6dbb
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2022-04-07 04:08:02 +0100

    added types to newscard

--- components/utils/NewsCard.tsx
+++ components/utils/NewsCard.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const NewsCard = ({ data }) => {
+const NewsCard: React.FC<{ data: any }> = ({ data }) => {
   return (
     <div
       key={data.id}
-      className="flex flex-col rounded-md border dark:border-gray-500 px-3 pt-3 pb-4"
+      className="flex flex-col rounded-md border px-3 pt-3 pb-4 dark:border-gray-500"
     >
       <h3 className="mb-2 text-lg font-semibold lg:text-base">
         {data.title.substr(0, 50)}...
@@ -14,7 +14,7 @@ const NewsCard = ({ data }) => {
         <a
           href={data.link}
           target={"_blank"}
-          className="grid dark:text-white hover:opacity-50 transition-all dark:border-gray-500 place-items-center rounded border py-2 px-3 text-black duration-200 hover:text-blue-500"
+          className="grid place-items-center rounded border py-2 px-3 text-black transition-all duration-200 hover:text-blue-500 hover:opacity-50 dark:border-gray-500 dark:text-white"
         >
           Read More
         </a>
